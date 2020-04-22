e3083d23efd1a30b7c89bda437d1876874804c4d
dndgus482 <dndgus4820@gmail.com>
2020-04-22 21:44:46 +0900

    Create articleList page

---
+++ android/app/src/main/java/com/example/android/activity/ArticleListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.R
+
+class ArticleListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_list)
+    }
+}
