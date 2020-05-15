06ac5ffbc042c54d463faeff437b550b7948bf61
dndgus482 <dndgus4820@gmail.com>
2020-05-15 23:38:09 +0900

    Make Bottomsheet to show list

--- android/app/src/main/java/com/example/android/activity/MainActivity.kt
+++ android/app/src/main/java/com/example/android/activity/MainActivity.kt
@@ -1,21 +1,68 @@
 package com.example.android.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.R
-import com.example.android.fragment.ItemFragment
+import com.example.android.fragment.MyItemRecyclerViewAdapter
+import com.example.android.helper.NetworkHelper
+import com.example.android.interfaces.LockBottomSheetBehavior
+import com.example.android.model.ArticlePreview
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_item_list.*
+import retrofit2.Call
+import retrofit2.Callback
 
-class MainActivity : AppCompatActivity(), ItemFragment.OnListFragmentInteractionListener {
+
+class MainActivity : AppCompatActivity(), OnListFragmentInteractionListener {
+
+    private var listener: OnListFragmentInteractionListener? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var articleListFragment = ItemFragment()
-        supportFragmentManager.beginTransaction().add(R.id.articleListContainer, articleListFragment).commit()
+        if(this is OnListFragmentInteractionListener)
+            listener = this
+        val behavior = BottomSheetBehavior.from(bottom_sheet)
 
-        main_button.setOnClickListener { articleListFragment.getList("코로나") }
+        main_button.setOnClickListener {
+            getList("코로나")
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED || newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    if (behavior is LockBottomSheetBehavior) {
+                        behavior.setLocked(true)
+                    }
+                }
+            }
+        })
+
+        button_expand.setOnClickListener {
+            if(behavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                button_expand.setImageResource(R.drawable.ic_expand_more_24px)
+            }
+            else if(behavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                button_expand.setImageResource(R.drawable.ic_expand_less_24px)
+
+            }
+        }
+        behavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
     override fun onListFragmentInteraction(id : Int) {
@@ -24,4 +71,57 @@ class MainActivity : AppCompatActivity(), ItemFragment.OnListFragmentInteraction
         startActivity(intent);
     }
 
+    fun getList(q : String) {
+        val recycle = recyclerView as RecyclerView
+
+        val errorMessage = {msg : String ->
+            val myToast: Toast = Toast.makeText(
+                this,
+                msg,
+                Toast.LENGTH_LONG
+            )
+            myToast.setGravity(Gravity.CENTER, 0, 0)
+            myToast.show()
+        }
+        var response = NetworkHelper.apiService.getList(q).enqueue(object :
+            Callback<ArticlePreview> {
+            override fun onFailure(call: Call<ArticlePreview>, t: Throwable) {
+                errorMessage("network Failure")
+                t.printStackTrace()
+            }
+
+
+            override fun onResponse(
+                call: Call<ArticlePreview?>,
+                response: retrofit2.Response<ArticlePreview?>
+            ) {
+                if (response.isSuccessful) {
+                    val result = response.body()
+                    val list = ArrayList<ArticlePreview.Item>();
+                    result?.let {
+                        for (x in result.items) {
+                            list.add(x)
+                        }
+                        if (recycle is RecyclerView) {
+                            with(recycle) {
+                                layoutManager = LinearLayoutManager(context)
+                                adapter = MyItemRecyclerViewAdapter(list, listener)
+                            }
+                        }
+                    }
+                    if(result == null || result.items.isEmpty()) errorMessage("받은 뉴스기사가 없습니다.")
+                }
+                else {
+                    errorMessage("${response.code()} error")
+                }
+            }
+        })
+    }
+
+
+}
+
+interface OnListFragmentInteractionListener {
+    // TODO: Update argument type and name
+    fun onListFragmentInteraction(id : Int)
 }
--- android/app/src/main/java/com/example/android/fragment/MyItemRecyclerViewAdapter.kt
+++ android/app/src/main/java/com/example/android/fragment/MyItemRecyclerViewAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.android.fragment
 
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,36 +8,42 @@ import android.widget.TextView
 import com.example.android.R
 
 
-import com.example.android.fragment.ItemFragment.OnListFragmentInteractionListener
 import com.example.android.model.ArticlePreview
 
 import kotlinx.android.synthetic.main.fragment_item.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MyItemRecyclerViewAdapter(
     private val mValues: List<ArticlePreview.Item>,
-    private val mListener: OnListFragmentInteractionListener?
+    private val mListener: com.example.android.activity.OnListFragmentInteractionListener?
 ) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
 
     private val mOnClickListener: View.OnClickListener
+    var today = Date();
+    val fmt = SimpleDateFormat("yyyy-MM-dd")
+    val fmtToday = SimpleDateFormat("HH-mm")
 
     init {
         mOnClickListener = View.OnClickListener { v ->
             val item = v.tag as ArticlePreview.Item
-            // Notify the active callbacks interface (the activity, if the fragment is attached to
-            // one) that an item has been selected.
             mListener?.onListFragmentInteraction(item.id)
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.fragment_item, parent, false)
+        today = Date()
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = mValues[position]
         holder.title.text = item.title
-        holder.pubDate.text = item.pubDate.toString();
+        if (fmt.format(item.pubDate) == fmt.format(today))
+            holder.pubDate.text = fmtToday.format(item.pubDate)
+        else
+            holder.pubDate.text = fmt.format(item.pubDate)
 
         with(holder.mView) {
             tag = item
