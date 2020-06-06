c94c4eb60f8ac17e9801553a86940688f31f97e6
dndgus482 <dndgus4820@gmail.com>
2020-06-06 20:16:08 +0900

    Update ArticleActivity.kt

--- android/app/src/main/java/com/example/android/activity/ArticleActivity.kt
+++ android/app/src/main/java/com/example/android/activity/ArticleActivity.kt
@@ -2,31 +2,44 @@ package com.example.android.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.room.Database
 import androidx.room.Room
 import com.example.android.R
 import com.example.android.model.AppDatabase
 import com.example.android.model.ArticlePreview
+import com.example.android.model.Bookmark
 import com.example.android.model.History
 import kotlinx.android.synthetic.main.activity_article.*
 
 
 class ArticleActivity : AppCompatActivity() {
 
+    private val db: AppDatabase by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries()
+            .build()
+    }
+
+    private val article: ArticlePreview by lazy {
+        intent.getSerializableExtra("id") as ArticlePreview
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
-
-
-        val article = intent.getSerializableExtra("id") as ArticlePreview
-
 
         if (Build.VERSION.SDK_INT >= 21) {
             article_webview.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW;
@@ -43,13 +56,6 @@ class ArticleActivity : AppCompatActivity() {
             }
         }
         article_webview.loadUrl(article.link)
-
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "database-name"
-        ).allowMainThreadQueries()
-            .build()
-
 
         db.runInTransaction {
             db.articleDao().insertAll(article)
@@ -69,7 +75,48 @@ class ArticleActivity : AppCompatActivity() {
                 onBackPressed()
                 true
             }
+
+            R.id.article_action_bookmark -> {
+                item.isChecked = !item.isChecked
+
+                if (item.isChecked) {
+                    item.setIcon(R.drawable.ic_bookmark_pink_24dp)
+                    addBookmark()
+                } else {
+                    item.setIcon(R.drawable.ic_bookmark_black_24dp)
+                    deleteBookmark()
+                }
+
+                true
+            }
+
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+
+        val bookmarked = db.bookmarkDao().findById(article.Unnamed)
+        if(bookmarked == null) {
+            menu?.getItem(0)?.isChecked = false
+        } else {
+            menu?.getItem(0)?.isChecked = true
+            menu?.getItem(0)?.setIcon(R.drawable.ic_bookmark_pink_24dp)
+        }
+
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    private fun addBookmark() {
+        db.runInTransaction {
+            db.articleDao().insertAll(article)
+            db.bookmarkDao().insertAll(Bookmark(article.Unnamed))
+        }
+    }
+
+    private fun deleteBookmark() {
+        db.runInTransaction {
+            db.bookmarkDao().delete(Bookmark(article.Unnamed))
         }
     }
 
